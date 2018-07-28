d438baeab5f55913f0ff86561cf3c51c84138e82
Pierre-Eric Garcia <darkaosis@gmail.com>
2018-07-28 19:24:04 +0200

    Plug velib stations API + compute nearest stations 🚲

--- src/components/map/index.js
+++ src/components/map/index.js
@@ -6,13 +6,15 @@ import {
   withScriptjs,
   withGoogleMap,
   GoogleMap,
-  DirectionsRenderer
+  DirectionsRenderer,
+  Marker
 } from "react-google-maps";
+import axios from "axios";
 
 export const Map = compose(
   withProps({
     googleMapURL:
-      "https://maps.googleapis.com/maps/api/js?key=AIzaSyBQ6VfBwTpWSgS0sDUp40JL_rV8LLx5Rkw",
+      "https://maps.googleapis.com/maps/api/js?key=AIzaSyBQ6VfBwTpWSgS0sDUp40JL_rV8LLx5Rkw&libraries=geometry,direction",
     loadingElement: <div style={{ height: `100%` }} />,
     containerElement: <div style={{ height: `60vh` }} />,
     mapElement: <div style={{ height: `100%` }} />
@@ -24,30 +26,147 @@ export const Map = compose(
       const {
         planning: { departure, arrival }
       } = this.props;
-      const DirectionsService = new google.maps.DirectionsService();
 
-      DirectionsService.route(
-        {
-          origin: new google.maps.LatLng(departure.lat, departure.lng),
-          destination: new google.maps.LatLng(arrival.lat, arrival.lng),
-          travelMode: google.maps.TravelMode.BICYCLING
-        },
-        (result, status) => {
-          console.log(result);
-          if (status === google.maps.DirectionsStatus.OK) {
-            this.setState({
-              directions: result
-            });
-          } else {
-            console.error(`error fetching directions ${result}`);
-          }
-        }
-      );
+      axios
+        .get(
+          "https://api.jcdecaux.com/vls/v1/stations?apiKey=d9358ba9d227951e83d301303262a22c47c05a26"
+        )
+        .then(response => {
+          const departureLocationStructured = new google.maps.LatLng({
+            lat: departure.lat,
+            lng: departure.lng
+          });
+          const arrivalLocationStructured = new google.maps.LatLng({
+            lat: arrival.lat,
+            lng: arrival.lng
+          });
+
+          const stationsStructured = response.data.map(station => ({
+            ...station,
+            position: new google.maps.LatLng({
+              lat: station.position.lat,
+              lng: station.position.lng
+            })
+          }));
+
+          const nearestDepartureStation = stationsStructured
+            .map(station => ({
+              ...station,
+              distance: google.maps.geometry.spherical.computeDistanceBetween(
+                station.position,
+                departureLocationStructured
+              )
+            }))
+            .filter(station => station.available_bikes > 0)
+            .sort(
+              (station, nextStation) => station.distance - nextStation.distance
+            )[0];
+
+          const nearestArrivalStation = stationsStructured
+            .map(station => ({
+              ...station,
+              distance: google.maps.geometry.spherical.computeDistanceBetween(
+                station.position,
+                arrivalLocationStructured
+              )
+            }))
+            .filter(station => station.available_bike_stands > 0)
+            .sort(
+              (station, nextStation) => station.distance - nextStation.distance
+            )[0];
+
+          this.setState({
+            nearestDepartureStation,
+            nearestArrivalStation,
+            stations: response.data
+          });
+
+          const DirectionsService = new google.maps.DirectionsService();
+
+          DirectionsService.route(
+            {
+              origin: departureLocationStructured,
+              destination: nearestDepartureStation.position,
+              travelMode: google.maps.TravelMode.WALKING
+            },
+            (result, status) => {
+              if (status === google.maps.DirectionsStatus.OK) {
+                this.setState({
+                  departureWalkingDirections: result
+                });
+              } else {
+                console.error(`error fetching directions ${result}`);
+              }
+            }
+          );
+
+          DirectionsService.route(
+            {
+              origin: nearestDepartureStation.position,
+              destination: nearestArrivalStation.position,
+              travelMode: google.maps.TravelMode.BICYCLING
+            },
+            (result, status) => {
+              if (status === google.maps.DirectionsStatus.OK) {
+                this.setState({
+                  bicyclingDirections: result
+                });
+              } else {
+                console.error(`error fetching directions ${result}`);
+              }
+            }
+          );
+
+          DirectionsService.route(
+            {
+              origin: nearestArrivalStation.position,
+              destination: arrivalLocationStructured,
+              travelMode: google.maps.TravelMode.WALKING
+            },
+            (result, status) => {
+              if (status === google.maps.DirectionsStatus.OK) {
+                this.setState({
+                  arrivalWalkingDirections: result
+                });
+              } else {
+                console.error(`error fetching directions ${result}`);
+              }
+            }
+          );
+        });
     }
   })
 )(props => (
   <GoogleMap defaultZoom={12} defaultCenter={{ lat: 48.866667, lng: 2.333333 }}>
-    {props.children}
-    <DirectionsRenderer directions={props.directions} />
+    <DirectionsRenderer
+      defaultOptions={{
+        suppressBicyclingLayer: true,
+        polylineOptions: {
+          strokeColor: "green",
+          strokeOpacity: 0.5
+        }
+      }}
+      directions={props.departureWalkingDirections}
+    />
+    <DirectionsRenderer
+      defaultOptions={{
+        suppressBicyclingLayer: true,
+        polylineOptions: {
+          strokeColor: "blue",
+          strokeOpacity: 0.5
+        }
+      }}
+      directions={props.bicyclingDirections}
+    />
+    <DirectionsRenderer
+      defaultOptions={{
+        suppressBicyclingLayer: true,
+        polylineOptions: {
+          strokeColor: "green",
+          strokeOpacity: 0.5
+        }
+      }}
+      directions={props.arrivalWalkingDirections}
+    />
   </GoogleMap>
 ));
--- src/components/planning-card/index.js
+++ src/components/planning-card/index.js
@@ -3,7 +3,6 @@ import "./styles.css";
 import React, { Component } from "react";
 import { Map } from "../map";
 import { withRouter } from "react-router-dom";
-import { Marker } from "react-google-maps";
 import idx from "idx";
 
 class DumbPlanningCard extends Component {
@@ -15,12 +14,12 @@ class DumbPlanningCard extends Component {
     } = this.props;
 
     const departureGeocodeSplitted = departureGeocode.split("_");
-    const departureLat = idx(departureGeocodeSplitted, _ => _[0]);
-    const departureLng = idx(departureGeocodeSplitted, _ => _[1]);
+    const departureLat = parseFloat(idx(departureGeocodeSplitted, _ => _[0]));
+    const departureLng = parseFloat(idx(departureGeocodeSplitted, _ => _[1]));
 
     const arrivalGeocodeSplitted = arrivalGeocode.split("_");
-    const arrivalLat = idx(arrivalGeocodeSplitted, _ => _[0]);
-    const arrivalLng = idx(arrivalGeocodeSplitted, _ => _[1]);
+    const arrivalLat = parseFloat(idx(arrivalGeocodeSplitted, _ => _[0]));
+    const arrivalLng = parseFloat(idx(arrivalGeocodeSplitted, _ => _[1]));
 
     return (
       <div className="planningCard card">
@@ -29,24 +28,7 @@ class DumbPlanningCard extends Component {
             departure: { lat: departureLat, lng: departureLng },
             arrival: { lat: arrivalLat, lng: arrivalLng }
           }}
-        >
-          {departureLat && departureLng ? (
-            <Marker
-              position={{
-                lat: parseFloat(departureLat),
-                lng: parseFloat(departureLng)
-              }}
-            />
-          ) : null}
-          {arrivalLat && arrivalLng ? (
-            <Marker
-              position={{
-                lat: parseFloat(arrivalLat),
-                lng: parseFloat(arrivalLng)
-              }}
-            />
-          ) : null}
-        </Map>
+        />
       </div>
     );
   }
